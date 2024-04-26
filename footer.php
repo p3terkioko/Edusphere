8eb603b64f8059f96519b7c5f60fbe66e8308b0f
Peter Kioko <peterkioko64@gmail.com>
2024-04-26 20:14:03 +0300

    initial commit

---
+++ footer.php
@@ -0,0 +1,63 @@
+		<!-- Footer
+		============================================= -->
+		<footer id="footer" class="dark">
+
+			
+
+			<!-- Copyrights
+			============================================= -->
+			<div id="copyrights">
+
+				<div class="container clearfix">
+
+					<div class="col_half">
+						Copyrights &copy; 2022 All Rights Reserved by Peter Kioko P15/144253/2022<br>
+					</div>
+
+					<div class="col_half col_last tright">
+						<div class="fright clearfix">
+							<a target="_blank" href="https://www.instagram.com/kio.ko_/" class="social-icon si-small si-borderless si-gplus">
+								<i class="icon-instagram"></i>
+								<i class="icon-instagram"></i>
+							</a>
+
+							<a target="_blank" href="https://github.com/p3terkioko" class="social-icon si-small si-borderless si-github">
+								<i class="icon-github"></i>
+								<i class="icon-github"></i>
+							</a>
+							<a target="_blank" href="mailto:peterkioko64@gmail.com" class="social-icon si-small si-borderless si-github">
+								<i class="icon-gmail"></i>
+								<i class="icon-gmail"></i>
+							</a>
+
+							
+						</div>
+
+						<div class="clear"></div>
+
+						<i class="icon-envelope2"></i> peterkioko64@gmail.com <span class="middot">&middot;</span> <i class="icon-headphones"></i> 0703844258 <span class="middot"></span>
+					</div>
+
+				</div>
+
+			</div>
+
+		</footer>
+
+	</div>
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,188 @@
+<?php include("header.php"); ?>
+
+<div class="container clearfix"></div>
+<!-- Page Sub Menu
+		============================================= -->
+		<div id="page-menu">
+			<div id="page-menu-wrap">
+			</div>
+		</div><!-- #page-menu end -->
+
+
+		<section id="slider" class="slider-parallax swiper_wrapper full-screen clearfix">
+			<div class="slider-parallax-inner">
+
+				<div class="swiper-container swiper-parent">
+					<div class="swiper-wrapper">
+						
+						<div class="swiper-slide dark">
+							<div class="container clearfix">
+								<div class="slider-caption slider-caption-center">
+									<h2 data-caption-animate="fadeInUp">This is EduSphere</h2>
+									<p data-caption-animate="fadeInUp" data-caption-delay="200">The Comprehensive Learning Repository</p>
+								</div>
+							</div>
+							<div class="video-wrap">
+								<img src="images/slider/slide.jpg">
+								<div class="video-overlay" style="background-color: rgba(0,0,0,0.55);"></div>
+							</div>
+						</div>
+						<div class="swiper-slide dark" style="background-image: url('images/slider/Slide3.jpg'); background-position: center top;">
+							<div class="container clearfix">
+								<div class="slider-caption">
+									<h2 data-caption-animate="fadeInUp">We are EduSphere</h2>
+									<p data-caption-animate="fadeInUp" data-caption-delay="200">The Comprehensive Learning Repository</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div id="slider-arrow-left"><i class="icon-angle-left"></i></div>
+					<div id="slider-arrow-right"><i class="icon-angle-right"></i></div>
+				</div>
+
+				<a href="#" data-scrollto="#content" data-offset="100" class="dark one-page-arrow"><i class="icon-angle-down infinite animated fadeInDown"></i></a>
+
+			</div>
+		</section>
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+
+			<div class="content-wrap">
+
+				<div class="container clearfix">
+					<div class="row clearfix">
+
+						<div class="col-lg-5">
+							<div class="heading-block topmargin">
+								<h1>Welcome to Edu <br>Sphere</h1>
+							</div>
+							<p class="lead">EduSphere is an innovative platform designed to revolutionize the way education is delivered. It serves as a comprehensive repository where teachers can upload a variety of educational resources, including courses, books, and video tutorials, organized into different categories for easy navigation. This platform aims to provide a personalized, comprehensive and research-driven platform for online learning.</p>
+						</div>
+
+						<div class="col-lg-7">
+
+							<div style="position: relative; margin-bottom: -60px;" class="ohidden" data-height-lg="426" data-height-md="567" data-height-sm="470" data-height-xs="287" data-height-xxs="183">
+								<img src="images/services/macbook.png" style="position: absolute; top: 0; left: 0;" data-animate="fadeInUp" data-delay="100" alt="Chrome">
+							</div>
+
+						</div>
+
+					</div>
+				</div>
+
+				<div class="section nobottommargin">
+					<div class="container clear-bottommargin clearfix">
+
+						<div class="row topmargin-sm clearfix">
+
+							<div class="col-md-4 bottommargin">
+								<i class="i-plain color i-large icon-smile inline-block" style="margin-bottom: 15px;"></i>
+								<div class="heading-block nobottomborder" style="margin-bottom: 15px;">
+									<h4>User Satisfaction</h4>
+								</div>
+							</div>
+
+							<div class="col-md-4 bottommargin">
+								<i class="i-plain color i-large icon-users inline-block" style="margin-bottom: 15px;"></i>
+								<div class="heading-block nobottomborder" style="margin-bottom: 15px;">
+									<h4>Promotes TeamWork</h4>
+								</div>
+							</div>
+
+							<div class="col-md-4 bottommargin">
+								<i class="i-plain color i-large icon-time inline-block" style="margin-bottom: 15px;"></i>
+								<div class="heading-block nobottomborder" style="margin-bottom: 15px;">
+									<h4>Promotes Time and Task Management</h4>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+				</div>
+
+				<div class="container clearfix" id="features">
+
+					<div class="row topmargin-lg bottommargin-sm">
+
+						<div class="heading-block center">
+							<h2>EduSphere</h2>
+							<span class="divcenter">Our team and instructors have many unique qualities &amp; skills. Dear students, We are providing free online learning platform. Where your skills are polished.</span>
+						</div>
+
+						<div class="col-md-4 col-sm-6 bottommargin">
+
+							<div class="feature-box fbox-right topmargin" data-animate="fadeIn">
+								<div class="fbox-icon">
+									<a href="#"><i class="icon-user4"></i></a>
+								</div>
+								<h3>Platform for Qualified, Passionate Instructors</h3>
+								<p>Edusphere provides a platform for qualified instructors to deliver content to learners</p>
+							</div>
+
+							<div class="feature-box fbox-right topmargin" data-animate="fadeIn" data-delay="200">
+							</div>
+
+							
+
+						</div>
+
+						<div class="col-md-4 hidden-sm bottommargin center">
+							<img src="images/services/iphone14.png" alt="iphone 2">
+						</div>
+
+						<div class="col-md-4 col-sm-6 bottommargin">
+
+							<div class="feature-box topmargin" data-animate="fadeIn">
+								<div class="fbox-icon">
+									<a href="#"><i class="icon-facetime-video"></i></a>
+								</div>
+								<h3>Video Lectures</h3>
+								<p>We are also providing international standard lectures on YouTube.</p>
+							</div>
+
+							<div class="feature-box topmargin" data-animate="fadeIn" data-delay="200">
+								<div class="fbox-icon">
+									<a href="#"><i class="icon-line-check"></i></a>
+								</div>
+								<h3>Endless Possibilities</h3>
+								<p>We are providing Endless Possibilities for students</p>
+							</div>
+
+							
+
+						</div>
+
+					</div>
+
+				</div>
+
+
+				
+				
+
+
+				<div class="clear"></div>
+
+				
+				
+
+				</div>
+
+				<div class="section notopmargin notopborder">
+					<div class="container clearfix">
+						<div class="heading-block center nomargin">
+							<h3>Latest from the Blog</h3>
+						</div>
+					</div>
+				</div>
+
+				
+									
+					</div>
+				
+		</section><!-- #content end -->
+
+<?php include("footer.php"); ?>
