9d614625c15aa2dee9d025e080f47757c9c6056e
Peter Kioko <peterkioko64@gmail.com>
2024-05-20 09:26:44 +0300

    added new sql backup

--- include/openconfig.php
+++ include/openconfig.php
@@ -2,7 +2,7 @@
 	
 	// if direct access to this page
 if(preg_match("/config.php/",$_SERVER['SCRIPT_FILENAME'])){
-	die("Access denied: Please away from here.");
+	die("Access denied: Please away stay from here.");
 }
 
 	$connection = mysqli_connect('localhost','root','','exceptional') or die("Database Not connected".mysqli_connect_error());
